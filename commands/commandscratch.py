5f867a814831384ad1871c3309810465b797aa5b
shardcore <shardcore@shardcore.org>
2012-08-16 17:01:28 +0100

    chilling the fuck out of the chill command

--- commands/commandscratch.py
+++ commands/commandscratch.py
@@ -25,7 +25,12 @@ class ChillCommand( BaseCommand ):
         BaseCommand.__init__( self )
         self.command_mappings = [ "chill" ]
         self.templates = [  Template("tells $name to chill the fuck out."),
-                            Template("tells the !Kaiser to chill the fuck out.") ]
+                            Template("tells the !Kaiser to chill the fuck out."),
+                            Template("can see that $name is already very chilled and tells the !Kaiser to chill the fuck out."),
+                            Template("bars $name for being too punchy."),
+                            Template("puts $name on the special un-chill list."),
+                            Template("pours everyone a !drink, on the house."),
+                            ]
 
     def generate( self, name ):
         template = random.choice( self.templates )
