1686fe1f0e39f3fde60300ccf61b972fc7be9e0c
mjays <martin@mjays.net>
2012-08-23 19:09:52 +0200

    !freud

--- commands/commandscratch.py
+++ commands/commandscratch.py
@@ -54,3 +54,18 @@ class PeterCommand( BaseCommand ):
         template = random.choice( self.templates )
         message_out = template.substitute(name=name)
         return "/me %s" % message_out
+
+class FreudCommand( BaseCommand ):
+
+    def __init__(self):
+        BaseCommand.__init__( self )
+        self.command_mappings = [ "freud" ]
+        self.templates = [  Template("asks $name to tell him about their mother"),
+                            Template("hands $name a cigar and asks how they feel about what they are sucking on."),
+                            Template("longs for a father figure.")                            
+                            ]
+
+    def generate( self, name ):
+        template = random.choice( self.templates )
+        message_out = template.substitute(name=name)
+        return "/me %s" % message_out
