21e3330ed11ae4ddb48bf47f9b7f977c548bc0f4
shardcore <shardcore@shardcore.org>
2012-11-15 11:01:07 +0000

    more louisa lies

--- commands/doriscommand.py
+++ commands/doriscommand.py
@@ -7,14 +7,24 @@ class DorisCommand( BaseCommand ):
 
     def __init__(self):
         BaseCommand.__init__( self )
-        self.command_mappings = [ "doris" ]
+        self.command_mappings = [ "doris", "louisa" ]
         self.templates = [  Template("drinks all the gin."),
                             Template("drinks !satan under the table."),
                             Template("goes for a road trip."),
-                            Template("drinks !satan under the table."),
                             Template("does the deckless !dovehand."),
                             Template("is shocked that $name unironically called himself a Futurist."),
-                            Template("reminds you of the Trojan Unicorn.") ]
+                            Template("likes guitars"),
+                            Template("is a secret goth"),
+                            Template("was born a poor black boy in Mississipi."),
+                            Template("starts a conference with !mjays"),
+                            Template("enjoys some quality time with mother."),
+                            Template("demonstrates the Heinrich manouver."),
+                            Template("is in two places at once."),
+                            Template("writes some words."),
+                            Template("reminds you of the Trojan Unicorn."),
+                            Template("likes the antlers.")
+                            
+ ]
 
     def generate( self, name ):
         template = random.choice( self.templates )
