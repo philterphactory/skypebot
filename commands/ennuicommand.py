c4bddecf75e235ef4573cdbb41a46d31ecbffd3e
mjays <martin@mjays.net>
2013-01-11 14:09:13 +0100

    bored

--- commands/ennuicommand.py
+++ commands/ennuicommand.py
@@ -9,13 +9,17 @@ class EnnuiCommand( BaseCommand ):
         BaseCommand.__init__( self )
         self.command_mappings = [ "boredom","ennui" ]
         self.templates = [  Template("draws the curtains and returns to bed."),
-                            Template("wonders why $name bothers."), 
-                            Template("dispairs of it all."), 
-                            Template("contemplates mortality."), 
-                            Template("gazes into the distance."), 
-                            Template("quotes Wilde: \"I am tired of myself to-night. I should like to be somebody else.\""), 
-                            Template("retreats from the world."), 
-                            Template("goes for a long walk alone.") 
+                            Template("wonders why $name bothers."),
+                            Template("starts to read a book, puts it away after the second page."),
+                            Template("visits the same website for the 15th time for no particular reason."),
+                            Template("idly switches the telly on and off."),
+                            Template("get's lost looking at the wall paper."),
+                            Template("despairs of it all."),
+                            Template("contemplates mortality."),
+                            Template("gazes into the distance."),
+                            Template("quotes Wilde: \"I am tired of myself to-night. I should like to be somebody else.\""),
+                            Template("retreats from the world."),
+                            Template("goes for a long walk alone.")
                             ]
 
     def generate( self, name ):
