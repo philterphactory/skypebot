ca222e9658b0698fce9bef6f1948e14b8a1ec852
Adam Hoyle <adam.junk@do-tank.co.uk>
2013-01-25 14:57:19 +0000

    oh, we are, are we?

--- commands/commandscratch.py
+++ commands/commandscratch.py
@@ -337,4 +337,22 @@ class KickCommand( BaseCommand ):
     def generate( self, name ):
         template = random.choice( self.templates )
         message_out = template.substitute(name=name)
-        return "/me %s" % message_out   
+        return "/me %s" % message_out
+        
+        
+class WeAreCommand( BaseCommand ):
+
+    def __init__(self):
+        BaseCommand.__init__( self )
+        self.command_mappings = [ "weare", "social", "twitter" ]
+        self.platform = [ "Facebook", "Twitter", "MySpace", "FriendsReunited" ]
+        self.templates = [  Template("starts another $platform campaign."),
+                            Template("designs another $platform app for $name."),
+                            Template("bores $name shitless with excited talk about the potential for $platform apps."),
+                            Template("considers making his own version of Nike+ called $name+"),
+                            ]
+
+    def generate( self, name ):
+        template = random.choice( self.templates )
+        message_out = template.substitute(name=name,platform=platform)
+        return "/me %s" % message_out
