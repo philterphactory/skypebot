b1f478170d2db08232d6207cb5f2e69c3de442f1
mjays <martin@mjays.net>
2013-01-30 12:31:39 +0100

    a couple. bit bored now.

--- commands/socialcommand.py
+++ commands/socialcommand.py
@@ -13,10 +13,16 @@ class WeAreCommand( BaseCommand ):
                             Template("designs another $platform app for $name."),
                             Template("bores $name shitless with excited talk about the potential for $platform apps."),
                             Template("considers making his own version of Nike+ called $name+"),
+                            Template("is convinced that $platform is the new $platform2."),
+                            Template("insists that $name place all their bets on the new $platform engagement strategy."),
+                            Template("wonders how to seed their new viral on $platform"),
+                            Template("looks for a community manager for $platform"),
+                            Template("tries to convince $name that $platform is not what they want, instead it's $platform2 they're looking for.")
                             ]
 
     def generate( self, name ):
         template = random.choice( self.templates )
         platform = random.choice( self.platforms )
-        message_out = template.substitute(name=name,platform=platform)
+        platform2 = random.choice(self.platforms)
+        message_out = template.substitute(name=name,platform=platform, platform2=platform2)
         return "/me %s" % message_out
