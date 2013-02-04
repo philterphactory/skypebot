cad227c7aaaf2cbb46d423ee2813ad1b066ea171
Adam Hoyle <adam.junk@do-tank.co.uk>
2013-02-04 17:30:45 +0000

    due to popular demand, here comes the smebs

--- commands/socialcommand.py
+++ commands/socialcommand.py
@@ -7,7 +7,7 @@ class WeAreCommand( BaseCommand ):
 
     def __init__(self):
         BaseCommand.__init__( self )
-        self.command_mappings = [ "weare", "social", "twitter" ]
+        self.command_mappings = [ "weare", "social", "twitter", "smebs", "pinterest" ]
         self.platforms = [ "Facebook", "Twitter", "MySpace", "FriendsReunited", "Friendster", "Instagram", "Path", "Vine", "Flickr", "ThisIsMyJam", "HypeMachine", "RunKeeper" ]
         self.templates = [  Template("starts another $platform campaign."),
                             Template("designs another $platform app for $name."),
