cbd80812b46f83a0331ab946811fe122101a9b2c
shardcore <shardcore@shardcore.org>
2013-02-13 22:08:38 +0000

    @purps is a lightweight

--- commands/lightweightcommand.py
+++ commands/lightweightcommand.py
@@ -9,7 +9,15 @@ class LightweightCommand( BaseCommand ):
         BaseCommand.__init__( self )
         self.command_mappings = [ "lightweight" ]
         self.templates = [  Template("grabs the mop and tuts at the mess $name has made"),
-                            Template("calls an ambulance for $name") ] 
+                            Template("calls an ambulance for $name"),
+                            Template("talks conspiratorially behind $name\s back after they leave."),
+                            Template("expected so much more from $name"),
+                            Template("de-invites $name from the forthcoming phactory bar coke and hookers party"),
+                            Template("thinks $name should learn to hold their !drink"),
+                            Template("calls $name a rude name"),
+                            Template("tells $name that even !purps lasted longer than this. !ffs")
+
+ ] 
 
     def generate( self, name ):
         template = random.choice( self.templates )
