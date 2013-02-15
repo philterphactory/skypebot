1c565ea6d4630ec9dbb93f8049a0b7246fb07e88
mjays <martin@mjays.net>
2013-02-15 16:52:04 +0100

    ffs povey

--- commands/commandscratch.py
+++ commands/commandscratch.py
@@ -362,7 +362,7 @@ class SacrumCommand( BaseCommand ):
     def __init__(self):
         BaseCommand.__init__( self )
         self.command_mappings = [ "Sacrum" ]
-        self.templates = [  Template("sends $name much warmth.")
+        self.templates = [  Template("sends $name much warmth."),
                             Template("is European man with skills in advertising."),
                             Template("has his own pencils."),
                             Template("got in to funky advertising agency."),
