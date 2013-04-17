540727cea491b67fbb39b448acbb6151e5b159b3
mjays <martin@mjays.net>
2013-04-17 11:14:14 +0200

    deduplicate birthday

--- commands/birthdaycommand.py
+++ commands/birthdaycommand.py
@@ -14,7 +14,9 @@ class BirthdayCommand( BaseCommand ):
 
         self.templates = [  Template("strings up the bunting and launches a party popper into $name's drink"),
                             Template("lights up some everlasting candles and shoves them into a pile of !snack"),
-                            Template("sombrely reminds all present that each birthday is a year closer to death before tweaking $name's nose and runnning off cackling into the cellar.") ]
+                            Template("sombrely reminds all present that each birthday is a year closer to death before tweaking $name's nose and runnning off cackling into the cellar."),
+                            Template("pops the champagne for $name."),
+                            Template("brings out the cake for $name.")]
     
     def generate( self, name ):
         template = random.choice( self.templates )
