2767e3de3edcd3aa1c675d9981170843458593e7
Adam Hoyle <adam.junk@do-tank.co.uk>
2013-04-22 22:20:02 +0100

    a different test name #tiime

--- commands/commandscratch.py
+++ commands/commandscratch.py
@@ -403,7 +403,7 @@ class TimeSensitiveCommand( BaseCommand ):
 
     def __init__(self):
         BaseCommand.__init__( self )
-        self.command_mappings = [ "timetest" ]
+        self.command_mappings = [ "tiime" ]
         self.templates = [  Template("looks at his watch, it's $hour."),
                             ]
 
