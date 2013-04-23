cb1bb6538df1392d591147f44e4c9414a27455e6
shardcore <shardcore@shardcore.org>
2013-04-23 15:10:14 +0100

    something clashy clashy in namespace?

---
+++ commands/whutcommand.py
@@ -0,0 +1,42 @@
+# coding=UTF-8
+
+from string import Template
+import random
+from commandbase import BaseCommand
+
+import pkgutil
+import commands
+
+class WhutCommand( BaseCommand ):
+
+    def __init__(self):
+        BaseCommand.__init__( self )
+        self.command_mappings = [ "cmd", "whut" ]
+        
+    def generate( self, name ):
+
+        # import commands
+        a_commands = []
+
+        ## load all commands
+        reload( commands )
+        for loader, modname, ispkg in pkgutil.iter_modules( commands.__path__, prefix="commands." ):
+        
+
+            module = __import__( modname, fromlist="dummy" )
+            reload( module )
+            for klassname in dir( module ):
+                if "Command" in klassname and "BaseCommand" not in klassname:
+                    
+                    
+                    kommandklass = getattr( module, klassname )
+                    kommand = kommandklass()
+                    a_commands.append( kommand )
+        out='commands: '
+        for cmd in a_commands:
+            l=cmd.command_mappings
+            for c in l:
+                out=out+c+' '
+            
+        message_out = out
+        return "/me %s" % message_out
